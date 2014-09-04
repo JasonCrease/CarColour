5b39d62c2a6d336cc4cc161deb41afd7722f6615
Jason Crease <jason.crease@red-gate.com>
2014-09-04 21:34:05 +0100

    first commit

---
+++ WpfProto/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace WpfProto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Processor m_Processor;
+        Timer m_Timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            m_Processor = new Processor();
+            m_Processor.BeforeImagePath = System.IO.Path.GetFullPath(".\\..\\..\\..\\Images\\car2.jpg");
+            m_Timer = new Timer(Update, null, 1000, 4000);
+        }
+
+        private void Update(object state)
+        {
+            m_Processor.Process(90);
+
+            Dispatcher.Invoke(new Action(() =>
+                ImageBefore.Source = BitmapSourceConvert.ToBitmapSource(m_Processor.BeforeImage)));          
+            Dispatcher.Invoke(new Action(() =>
+                ImageAfter.Source = BitmapSourceConvert.ToBitmapSource(m_Processor.AfterImage)));
+
+        }
+        private void ButtonGo_Click(object sender, RoutedEventArgs e)
+        {
+            m_Processor.Process(40);
+
+            ImageBefore.Source = BitmapSourceConvert.ToBitmapSource(m_Processor.BeforeImage);
+            ImageAfter.Source = BitmapSourceConvert.ToBitmapSource(m_Processor.AfterImage);
+        }
+
+        public static class BitmapSourceConvert
+        {
+            [DllImport("gdi32")]
+            private static extern int DeleteObject(IntPtr o);
+
+            public static BitmapSource ToBitmapSource(Emgu.CV.IImage image)
+            {
+                using (System.Drawing.Bitmap source = image.Bitmap)
+                {
+                    IntPtr ptr = source.GetHbitmap();
+
+                    BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                        ptr,
+                        IntPtr.Zero,
+                        Int32Rect.Empty,
+                        System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                    DeleteObject(ptr);
+                    return bs;
+                }
+            }
+        }
+    }
+}
---
+++ WpfProto/Processor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace WpfProto
+{
+    class Processor
+    {
+        public Emgu.CV.Image<Bgr, byte> BeforeImage { get; private set; }
+        public Emgu.CV.Image<Hsv, byte> DebugImage { get; private set; }
+        public Emgu.CV.Image<Bgr, byte> AfterImage { get; private set; }
+        public string BeforeImagePath { get; set; }
+
+        internal void Process(int hueAfter)
+        {
+            byte afterHue = (byte)hueAfter;
+            byte afterSat = 0;
+
+            BeforeImage = new Image<Bgr, byte>(BeforeImagePath).
+                Resize(440, 320, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC, false);
+            DebugImage = BeforeImage.Convert<Hsv,byte>();
+
+            for (int i = 0; i < DebugImage.Width; i++)
+                for (int j = 0; j < DebugImage.Height; j++)
+                {
+                    int hue = DebugImage.Data[j, i, 0];
+                    int sat = DebugImage.Data[j, i, 1];
+                    int val = DebugImage.Data[j, i, 2];
+
+                    if ((hue < 10 || hue > 170))
+                    {
+                        //DebugImage.Data[j, i, 0] = afterHue;
+                        //DebugImage.Data[j, i, 1] = afterSat;
+
+                        if (val > 140)
+                        {
+                            DebugImage.Data[j, i, 1] = 0;
+                        }
+                        else
+                        {
+                            //DebugImage.Data[j, i, 1] = 0;
+                            DebugImage.Data[j, i, 1] = 0;
+                        }
+                    }
+                }
+
+          AfterImage = DebugImage.Convert<Bgr, byte>();
+        }
+    }
+}
